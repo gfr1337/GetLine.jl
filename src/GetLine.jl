016bebbeca13fc78c52fac384fceb9fc6d5c8271
George Rogers <george.f.rogers@gmail.com>
2023-05-23 07:25:07 -0400

    initial commit

---
+++ src/GetLine.jl
@@ -0,0 +1,17 @@
+module GetLine
+
+"""
+    getline(in, default=nothing)
+
+Reads a line removing whitespace, if eof is reached returns `default`
+"""
+function getline(in=stdin; default=nothing)
+    line = readline(in, keep=true) 
+    if line === ""
+        default
+    else
+        chomp(line)
+    end
+end
+
+end # module GetLine
